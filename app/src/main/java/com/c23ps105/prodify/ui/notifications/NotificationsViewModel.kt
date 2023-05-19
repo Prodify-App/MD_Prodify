46707ce411614286090b3f9c61feea4233d1a27a
Hamza Firdaus <elhamzafirdaus@gmail.com>
2023-05-19 18:20:48 +0700

    Initial commit

---
+++ app/src/main/java/com/c23ps105/prodify/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.c23ps105.prodify.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
