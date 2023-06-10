5da84914f580d71b1d48a7b972a9e7d64da20606
Hamza Firdaus <elhamzafirdaus@gmail.com>
2023-06-10 10:28:45 +0700

    Adding Header Interceptor for Token Bearer

--- app/src/main/java/com/c23ps105/prodify/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/c23ps105/prodify/data/remote/retrofit/ApiConfig.kt
@@ -1,6 +1,5 @@
 package com.c23ps105.prodify.data.remote.retrofit
 
-import android.util.Log
 import com.c23ps105.prodify.helper.SessionPreferences
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -10,22 +9,21 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    fun getApiService(sessionPreferences: SessionPreferences): ApiService {
+    fun getApiService(sessionPreference: SessionPreferences): ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .addInterceptor { chain ->
-                val originalRequest = chain.request()
-                val tokenFlow = sessionPreferences.getToken()
+                val request = chain.request()
+                val tokenFlow = sessionPreference.getToken()
 
                 val token = runBlocking { tokenFlow.first() }
-                val modifiedRequest = originalRequest.newBuilder()
+                val requestWithHeader = request.newBuilder()
                     .header("Authorization", "Bearer $token")
                     .build()
 
-                val response = chain.proceed(modifiedRequest)
-                Log.d("testing", response.code.toString())
+                val response = chain.proceed(requestWithHeader)
                 response
             }
             .build()
